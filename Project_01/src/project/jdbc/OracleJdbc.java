edb7f8deb9606c454f41f6601d6d8fe068fd2a54
itwill <itwill@DESKTOP-OD6KHCN>
2024-05-08 20:42:54 +0900

    project01

--- Project_01/src/project/jdbc/OracleJdbc.java
+++ Project_01/src/project/jdbc/OracleJdbc.java
@@ -2,10 +2,13 @@ package project.jdbc;
 
 public interface OracleJdbc {
 	
-	String URL = "jdbc:oracle:thin:@localhost:1521:xe";
+	// String URL = "jdbc:oracle:thin:@localhost:1521:xe"; 
+	String URL = "jdbc:oracle:thin:@192.168.20.21:1521:xe";
 	
-	String USER = "SYSTEM";
+	// String USER = "SYSTEM";
+	String USER = "scott";
 	
-	String PASSWORD = "0831";
+	// String PASSWORD = "0831";
+	String PASSWORD = "tiger";
 
 }
